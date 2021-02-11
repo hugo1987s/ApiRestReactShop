7e02d8b7766c548cc6d484403245a225cc274a30
hugo1987s <hugo.schreiber@hasar.com>
2021-02-11 18:22:17 -0300

    Add BodyParser

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const server = express()
-const serverPort = 4500
+const serverPort = process.env.PORT || 4500
+
+server.use(bodyParser.urlencoded({ extended: false}))
+server.use(bodyParser.json())
 
 server.listen(serverPort, () => {
-    console.log(`Server running at port ${serverPort}.`);
+    console.log(`Server running at port ${serverPort}`)
 })
