304a21e1f96fe367aa9685f8963decded7879aa9
hugo1987s <hugo.schreiber@hasar.com>
2021-02-11 17:17:40 -0300

    Initial server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Initial");
+import express from 'express'
+
+const server = express()
+const serverPort = 4500
+
+server.listen(serverPort, () => {
+    console.log(`Server running at port ${serverPort}.`);
+})
