5f7479749164d82ed4ac1309deeed149f69ab564
hugo1987s <hugo.schreiber@hasar.com>
2021-02-12 21:23:29 -0300

    Rename config file

---
+++ configs/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyBpCyUW0lFV83tLTzHdTVFlIs96FfBMW-U",
+    authDomain: "cursoutn-434c2.firebaseapp.com",
+    projectId: "cursoutn-434c2",
+    storageBucket: "cursoutn-434c2.appspot.com",
+    messagingSenderId: "948760927930",
+    appId: "1:948760927930:web:1b581a0d75d8cdd0582c83"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+
+export default firebase
